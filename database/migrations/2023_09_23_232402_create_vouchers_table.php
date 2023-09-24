22ad7e2c081ffa9294646437d5e7dc3d1f41897d
Renato Morales <104334139+naatox@users.noreply.github.com>
2023-09-23 22:55:31 -0300

    base de datos

---
+++ database/migrations/2023_09_23_232402_create_vouchers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->integer('bookingCode');
+            $table->string('tripOrigin');
+            $table->string('tripDestination');
+
+            $table->primary('bookingCode');
+            $table->foreign(['tripOrigin', 'tripDestination'])->references(['origin','destination'])->on('trips');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vouchers');
+    }
+};
