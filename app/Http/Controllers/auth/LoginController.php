14b0114749380636cdf7cfe23956c1eb688c7e65
Renato Morales <104334139+naatox@users.noreply.github.com>
2023-09-30 19:54:18 -0300

    USU-001-Complete

--- app/Http/Controllers/auth/LoginController.php
+++ app/Http/Controllers/auth/LoginController.php
@@ -4,15 +4,19 @@ namespace App\Http\Controllers\auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Helpers\MyHelper;
+
 
 class LoginController extends Controller
 {
     //
 
 
+
     public function store(Request $request)
     {
+
+
+
         $messages = makeMessages();
 
         $this->validate($request, [
@@ -22,12 +26,15 @@ class LoginController extends Controller
 
 
 
-        if(!auth()->attempt($request->only('email', 'password'), $request->remember)){
+        if(!auth()->attempt([
+            'email'=> $request->email,
+            'password' => $request->password
+        ])){
             return back()->with('message', 'usuario no registrado o contraseña incorrecta');
         }
 
 
         //Ingresar ruta del cargar viajes
-        return redirect()->route('test');
+        return redirect()->route('welcome');
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+
 
 return new class extends Migration
 {
@@ -12,11 +14,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
+            $table->id();
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-
+            $table->rememberToken();
+            $table->timestamps();
 
         });
+
+        User::create([
+            'email'=> 'italo.donoso@ucn.cl',
+            'password' =>'Turjoy91'
+        ]);
     }
 
     /**
