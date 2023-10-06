0a373a7513989be9ce8a77d2f87820e65b96922a
Frankoyusta <Franko.yusta@alumnos.ucn.cl>
2023-10-06 00:29:24 -0300

    Loyout app.blade listo

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -4,62 +4,103 @@ namespace App\Http\Controllers;
 
 use App\Models\Trip;
 use Illuminate\Http\Request;
+use App\Imports\TravelsImport;
+use Maatwebsite\Excel\Facades\Excel;
 
-class TripController extends Controller
+class TravelController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function indexAddTravels()
     {
-        //
+
+        if (session('validRows') || session('invalidRows') || session('duplicatedRows')) {
+            session()->put('validRows', []);
+            session()->put('invalidRows', []);
+            session()->put('duplicatedRows', []);
+        } else {
+            session(['validRows' => []]);
+            session(['invalidRows' => []]);
+            session(['duplicatedRows' => []]);
+        }
+
+        return view('admin.travel.index', [
+            'validRows' => session('validRows'),
+            'invalidRows' => session('invalidRows'),
+            'duplicatedRows' => session('duplicatedRows')
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function indexTravels()
     {
-        //
+        return view('admin.travel.index', [
+            'validRows' => session('validRows'),
+            'invalidRows' => session('invalidRows'),
+            'duplicatedRows' => session('duplicatedRows')
+        ]);
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function travelCheck(Request $request)
     {
-        //
-    }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Trip $trip)
-    {
-        //
-    }
+        //Validar el archivo general
+        $messages = makeMessages();
+        $this->validate($request, [
+            'document' => ['required', 'max:5120', 'mimes:xlsx'],
+        ], $messages);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Trip $trip)
-    {
-        //
-    }
+        //Validar el archivo excel en detalle
+        if ($request->hasFile('document')) {
+            $file = request()->file('document');
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Trip $trip)
-    {
-        //
-    }
+            $import = new TravelsImport();
+            Excel::import($import, $file);
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Trip $trip)
-    {
-        //
+            // Obtener filas válidas e inválidas
+            $validRows = $import->getValidRows();
+            $invalidRows = $import->getInvalidRows();
+            $duplicatedRows = $import->getDuplicatedRows();
+
+            // dd($validRows, $invalidRows, $duplicatedRows);
+
+            // Agregar o actualizar las filas en la base de datos
+            foreach ($validRows as $row) {
+                $origin = $row['origen'];
+                $destination = $row['destino'];
+
+                // Verifica si la fila ya existe en la base de datos
+                $travel = Travel::where('origin', $origin)
+                    ->where('destination', $destination)
+                    ->first();
+
+                if ($travel) {
+                    // Si existe, realiza una actualización
+                    $travel->update([
+                        'seat_quantity' => $row['cantidad_de_asientos'],
+                        'base_rate' => $row['tarifa_base'],
+                    ]);
+                } else {
+                    // Si no existe, inserta un nuevo viaje a la base de datos
+                    Travel::create([
+                        'origin' => $origin,
+                        'destination' => $destination,
+                        'seat_quantity' => $row['cantidad_de_asientos'],
+                        'base_rate' => $row['tarifa_base'],
+                    ]);
+                }
+            }
+
+            //Eliminar registros (filas) vacios del  documento excel
+            $invalidRows = array_filter($invalidRows, function ($invalidrow) {
+                return $invalidrow['origen'] !== null || $invalidrow['destino'] !== null || $invalidrow['cantidad_de_asientos'] !== null || $invalidrow['tarifa_base'] !== null;
+            });
+
+            // dd(session('invalidRows'));
+
+            session()->put('validRows', $validRows);
+            session()->put('invalidRows', $invalidRows);
+            session()->put('duplicatedRows', $duplicatedRows);
+
+            // dd(count(session('validRows')), count(session('invalidRows')));
+
+            return redirect()->route('travelsAdd.index');
+        }
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,38 @@
 @extends('layouts.app')
 
-
+@section('title')
+Home Page
+@endsection
 @section('content')
+    <H1>ANASHE</H1>
+    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 
+          @auth
+              <!-Dentro de esta etiqueta solo lo ve el usuario autenticado->
+              <a href="{{route('welcome')}}" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Inicio</a>
+              </li>
+              <li>
+                <a href="{{route('logout')}}" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Logout</a>
+              </li>
+              <li>
+                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Proximamente</a>
+              </li>
+          @endauth
+
+          @guest
+            <!-Dentro de esta etiqueta solo lo ve el usuario invitado->
+            <li>
+                <a href="{{route('welcome')}}" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Inicio</a>
+              </li>
+              <li>
+                <a href="{{route('login')}}" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Login</a>
+              </li>
+              <li>
+                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Proximamente</a>
+              </li>
+          @endguest
+
+        </ul>
+      </div>
 @endsection
