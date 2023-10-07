f59ee28994df7053ae73238af1cf71c018a87e6e
Frankoyusta <Franko.yusta@alumnos.ucn.cl>
2023-10-07 12:34:45 -0300

    Interfaz grafica de Homepage, lista
    Igual a mockup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,48 +1,46 @@
 @extends('layouts.app')
 
 @section('title')
-Home Page
+Página principal
 @endsection
 @section('content')
     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
         <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
           @auth
               <!-Dentro de esta etiqueta solo lo ve el usuario autenticado->
-            <div class="bg-gray-custom text-white w-1/4 h-100 p-5">
+            <div class="bg-gray-custom-150  w-1/4 h-100 p-5">
             <li>
             <a href="{{route('welcome')}}" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Inicio</a>
             </li>
             <li>
-            <a href="{{route('logout')}}" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Logout</a>
+            <a href="{{route('logout')}}" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Salir de la sesión</a>
             </li>
             <li>
             <li>
-            <a href="{{route('index')}}" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Proximamente</a>
+            <a href="{{route('index')}}" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Cargar rutas de viaje</a>
             </li>
         </div>
           @endauth
           @guest
             <!-Dentro de esta etiqueta solo lo ve el usuario invitado->
-            <div class="bg-color: #EAEAEA text-white h-3/4 w-1/4 p-6">
+            <div class="bg-gray-custom-100  text-white h-4/4 w-[300px] p-6">
             <li>
-            <a href="{{route('welcome')}}" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Inicio</a>
+            <a href="{{route('login')}}" class="block mt-20 ml-[-24px] bg-gray-custom-150 w-[300px] h-[50px] py-2 pl-3 pr-4 text-gray-900 text-4xl font-normal hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Iniciar Sesión</a>
               </li>
-              <li>
-                <a href="{{route('login')}}" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Iniciar Sesión</a>
-              </li>
-              <li>
-                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Proximamente</a>
-              </li>
+            </div>
+            <div class= "h-screen relative">
+              <a href="#" class=" bg-green-custom text-8xl p-8 font-normal absolute left-[550px] top-[40%]">Próximamente</a>
+              <a class=" text-6xl p-8 inline absolute left-[630px] top-[29%]">Reservar</a>
+              <a class=" text-6xl p-8 inline absolute left-[880px] top-[29%]">pasajes</a>
             </div>
           @endguest
         </ul>
-
-
-        <div class="fixed bottom-0 left-0 w-full p-12 bg-blue-custom-50 dark:bg-gray-700 ">
-        <div class="container-fluid">
-            <a class="navbar-brand text-white text-2xl" href="#">Fono Ayuda: +56939309938</a>
-        </div>
-        </nav>
-        
+        <footer class="bg-blue-custom-50 text-white p-4">
+          <nav class="flex justify-between">
+            <a href="#" class="hover:text-gray-300">Fono ayuda: +552 546735</a>
+            <a class="hover:text-gray-300 text-center ">2023 - {{ now()->year }} | Todos los derechos de Turjoy estan resevados</a>
+            <a href="#" class="hover:text-gray-300"></a>
+          </nav>
+        </footer>
       </div>
 @endsection
