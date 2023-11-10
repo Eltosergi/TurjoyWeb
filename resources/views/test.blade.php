0cba8e84775261a47ce580a9a5e968ae8b101a50
Shhapa <thomas.chapa@alumnos.ucn.cl>
2023-11-10 11:40:48 -0300

    Update test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -16,7 +16,7 @@
 </div>
               <form class="space-y-4 md:space-y-6" action="#">
                   <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fecha del viaje:</label>
                       <input type="date" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
                   </div>
                   <div>
