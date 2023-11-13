3ce9903e5b56a0e9d9eee169616494ca26ef64fe
Frankoyusta <Franko.yusta@alumnos.ucn.cl>
2023-11-12 22:20:47 -0300

    feat: 3/5 tests incorporated

--- tests/Unit/SearchTicketTest.php
+++ tests/Unit/SearchTicketTest.php
@@ -1,16 +1,52 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Feature;
 
-use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Ticket;
+use App\Models\Trip;
+use App\Models\Voucher;
+use App\Http\Controllers\SearchController;
 
-class SearchTicketTest extends TestCase
+class searchReserveTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     */
-    public function test_example(): void
-    {
-        $this->assertTrue(true);
-    }
+    use RefreshDatabase;
+
+    public function testSuccessfulSearch()
+{
+    // Crear instancias de modelos necesarios para la prueba
+    $trip = Trip::create([
+        'origin' => 'Antofagasta',
+        'destination' =>'Calama',
+        'qtySeats' =>'28',
+        'price' => '1000',
+    ]);
+    $ticket = Ticket::create([
+        'code' => 'ABC123', 
+        'tripId' => $trip->id,
+        'seat' => '2',
+        'total'=>'2000',
+        'date' =>'2023-12-11',
+    ]);
+    $voucher = Voucher::create([
+        'ticketId' => $ticket->id,
+        'uri' =>'uriExample',
+        'date' => '2023-11-12',
+    ]);
+
+    // Realizar una solicitud simulada al endpoint 'search.result' del controlador
+    $response = $this->post(route('search.result'), ['code' => 'ABC123']);
+
+    $response->assertStatus(200);
+
+    // Verificar que la vista tiene los datos esperados
+    $response->assertViewHas('ticket', $ticket);
+    $response->assertViewHas('trip', $trip);
+    $response->assertViewHas('voucher', $voucher);
+}
+
+    
+
 }
---
+++ tests/Unit/loginTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+use App\Models\User;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /**
+     * Test a successful login.
+     *
+     * @return void
+     */
+    public function testSuccessfulLogin()
+    {
+        $password = $this->faker->password;
+        $user = User::create([
+            'name' => 'Italo Donoso',
+            'email' => 'italo.donoso@ucn.cl',
+            'role' => 'admin',
+            'password' => 'Turjoy91',
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => 'italo.donoso@ucn.cl',
+            'password' => 'Turjoy91',
+        ]);
+
+        $response->assertRedirect('/');
+        $this->assertAuthenticatedAs($user);
+    }
+
+    /**
+     * Test a failed login.
+     *
+     * @return void
+     */
+    public function testFailedLogin()
+    {
+        $response = $this->post('/login', [
+            'email' => 'fake@ucn.cl',
+            'password' => 'fake123',
+        ]);
+
+        $response->assertStatus(302);
+        $this->assertGuest();
+    }
+}
